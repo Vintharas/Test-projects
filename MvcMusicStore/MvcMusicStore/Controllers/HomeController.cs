cbfebec839d30c0c80a6c8f51641201329b00d0b
Vintharas <jaime.gg83@gmail.com>
2012-02-19 19:48:32 +0100

    Tinkered with MvcMusicStore.

--- MvcMusicStore/MvcMusicStore/Controllers/HomeController.cs
+++ MvcMusicStore/MvcMusicStore/Controllers/HomeController.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcMusicStore.Models;
 
 namespace MvcMusicStore.Controllers
 {
     public class HomeController : Controller
     {
+
+        private MusicStoreDB db = new MusicStoreDB();
+
         public ActionResult Index()
         {
             ViewBag.Message = "Welcome to ASP.NET MVC!";
@@ -18,6 +23,15 @@ namespace MvcMusicStore.Controllers
         public ActionResult About()
         {
             return View();
+        }
+
+        public ActionResult Search(string q)
+        {
+            var albums = db.Albums
+                .Include("Artist")
+                .Where(a => a.Title.Contains(q) || q == null)
+                .Take(10);
+            return View(albums);
         }
     }
 }
--- MvcMusicStore/MvcMusicStore/Models/Genre.cs
+++ MvcMusicStore/MvcMusicStore/Models/Genre.cs
@@ -4,7 +4,7 @@ namespace MvcMusicStore.Models
 {
     public class Genre
     {
-        public virtual string GenreId { get; set; }
+        public virtual int GenreId { get; set; }
         public virtual string Name { get; set; }
         public virtual string Description { get; set; }
         public virtual List<Album> Albums { get; set; }
