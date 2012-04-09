74d75dc7bf834adde92a1f542635b709a93b1a24
Vintharas <jaime.gg83@gmail.com>
2012-04-09 19:39:57 +0200

    Added examples on attached properties and data binding

---
+++ IllustratedWPF/Chapter7_AttachedProperties/Chapter7_AttachedProperties/IntStorage.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+namespace Chapter7_AttachedProperties
+{
+    /// <summary>
+    /// Target class of the attached property.
+    /// The only requirement for a target class that's going to
+    /// support the use of attached properties is that it inherits from DependencyObject
+    /// </summary>
+    public class IntStorage : DependencyObject
+    {
+         
+    }
+}
--- IllustratedWPF/Chapter7_AttachedProperties/Chapter7_AttachedProperties/MainWindow.xaml.cs
+++ IllustratedWPF/Chapter7_AttachedProperties/Chapter7_AttachedProperties/MainWindow.xaml.cs
@@ -19,9 +19,45 @@ namespace Chapter7_AttachedProperties
     /// </summary>
     public partial class MainWindow : Window
     {
+        // This is the attached property
+        public static readonly DependencyProperty CountProperty;
+
+        static MainWindow() // Static constructor
+        {
+            // Notice the use of the "RegisterAttached" method instead of the
+            // "Register" method used with regular dependency properties
+            CountProperty = DependencyProperty.RegisterAttached("Count", typeof (int), typeof (MainWindow));
+        }
+
+        public static int GetCount(IntStorage ints)
+        {
+            return (int) ints.GetValue(CountProperty);
+        }
+
+        public static void SetCount(IntStorage ints, int value)
+        {
+            ints.SetValue(CountProperty, value);
+        }
+
+
         public MainWindow()
         {
             InitializeComponent();
+
+            // Create targets programmatically and
+            // see how they store/retrieve a value for an attached proprerty
+            IntStorage is1 = new IntStorage();
+            IntStorage is2 = new IntStorage();
+            // Store values
+            SetCount(is1, 28);
+            SetCount(is2, 500);
+            // Retrieve the values
+            int i1 = GetCount(is1);
+            int i2 = GetCount(is2);
+            // Display the values
+            txt1.Text = i1.ToString();
+            txt2.Text = i2.ToString();
         }
+
     }
 }
