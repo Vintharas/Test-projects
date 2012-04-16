1ca8209a46730736c351cbb86088efaede5a0a46
Vintharas <jaime.gg83@gmail.com>
2012-04-16 18:07:30 +0200

    Added some examples about WPF styles

---
+++ WPF/IllustratedWPF/Chapter11_AssemblyResources/Chapter11_AssemblyResources/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Chapter11_AssemblyResources
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            /*
+            // Accessing to an Assembly Resource via code
+            Uri uri = new Uri("jaime.png", UriKind.Relative);
+            BitmapImage bitmapImage = new BitmapImage(uri);
+            Image image = new Image();
+            image.Source = bitmapImage;
+            // This would add a new image element
+            theGrid.Children.Add(image);
+            */
+        }
+    }
+}
