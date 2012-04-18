bef5fa305037eac1914cbb67dd78233f580dc467
Vintharas <jaime.gg83@gmail.com>
2012-04-18 17:49:41 +0200

    Started WP7 Home Inventory app from the book, but... the book is too simple and disorganized for my taste.

---
+++ WP7/MigratingToWP7/WP7HomeInventory/WP7HomeInventory/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows;
+
+using Microsoft.Phone.Controls;
+
+namespace WP7HomeInventory
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCategories_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Categories.xaml", UriKind.Relative));
+        }
+    }
+}
