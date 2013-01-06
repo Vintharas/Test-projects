cacb70a652e56e25485a26a0562559e68c13a0c6
vintharas <jaime.gg83@gmail.com>
2013-01-06 15:53:34 +0100

    Beginning Windows 8 App Dev: Added simple sample about Styles and DataTemplates

---
+++ Windows8/BeginningWindows8-KyleBurns/Chapter7_BuildingTheUI/Chapter7_BuildingTheUI/DummyData.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Chapter7_BuildingTheUI
+{
+    public class DummyData
+    {
+        public List<Item> Items { get; set; } 
+    }
+
+    public class Item
+    {
+        public string Title { get; set; }
+    }
+}
---
+++ Windows8/BeginningWindows8-KyleBurns/Chapter7_BuildingTheUI/Chapter7_BuildingTheUI/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Chapter7_BuildingTheUI
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            DataContext = new DummyData
+                {
+                    Items = new List<Item>
+                        {
+                            new Item {Title = "Item 1"},
+                            new Item {Title = "Item 2"},
+                            new Item {Title = "Item 3"},
+                            new Item {Title = "Item 4"}
+                        }
+                };
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.  The Parameter
+        /// property is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+    }
+}
