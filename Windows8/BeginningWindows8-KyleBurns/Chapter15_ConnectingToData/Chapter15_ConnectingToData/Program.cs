fa7d6f16c7a774d30a8eeb3562a15f6bbaffa2f5
vintharas <jaime.gg83@gmail.com>
2013-01-13 15:21:59 +0100

    Updated Beginning Windows 8 App Dev samples: Added small sample of consuming an OData service.

---
+++ Windows8/BeginningWindows8-KyleBurns/Chapter15_ConnectingToData/Chapter15_ConnectingToData/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter15_ConnectingToData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var service = new ODataDemo.DemoService(
+                new Uri("http://services.odata.org/OData/OData.svc"));
+            service.Products.ToList().ForEach(p => Console.WriteLine("{0} - {1}", p.Name, p.Price));
+            Console.ReadLine();
+        }
+    }
+}
