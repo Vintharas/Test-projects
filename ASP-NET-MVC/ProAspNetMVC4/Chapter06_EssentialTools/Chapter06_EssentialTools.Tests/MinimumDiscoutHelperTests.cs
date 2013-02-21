69e085b4906c6d58548814a729ad482204c29d57
vintharas <jaime.gg83@gmail.com>
2013-02-21 22:43:32 +0100

    Pro ASP.NET MVC 4: Updated example of IoC implementation with some basic unit tests.

---
+++ ASP-NET-MVC/ProAspNetMVC4/Chapter06_EssentialTools/Chapter06_EssentialTools.Tests/MinimumDiscoutHelperTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Chapter06_EssentialTools.Infrastructure;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Chapter06_EssentialTools.Tests
+{
+    [TestClass]
+    public class MinimumDiscoutHelperTests
+    {
+        [TestMethod]
+        public void ApplyDiscount_WhenTotalIsAbove100_ThenDiscountShouldBeTenPercent()
+        {
+            // Arrange
+            var discountHelper = GetDiscountHelper();
+            decimal total = 200;
+            // Act
+            var discountedTotal = discountHelper.ApplyDiscount(total);
+            // Assert
+            Assert.AreEqual(expected: 180, actual: discountedTotal);
+        }
+        
+        [TestMethod]
+        public void ApplyDiscount_WhenTotalIsBetween100And10Inclusive_ThenDiscountFiveDollars()
+        {
+            // Arrange
+            var discountHelper = GetDiscountHelper();
+            decimal total = 50;
+            // Act
+            var discountedTotal = discountHelper.ApplyDiscount(total);
+            // Assert
+            Assert.AreEqual(expected: 45, actual: discountedTotal);
+        }
+
+        [TestMethod]
+        public void ApplyDiscount_WhenTotalIsSmallerThan10Dollars_ThenNoDiscountWillBeApplied()
+        {
+            // Arrange
+            var discountHelper = GetDiscountHelper();
+            decimal total = 5;
+            // Act
+            var discountedTotal = discountHelper.ApplyDiscount(total);
+            // Assert
+            Assert.AreEqual(expected: total, actual: discountedTotal);
+        }
+
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        [TestMethod]
+        public void ApplyDiscount_WhenTotalIsNegative_ThenShouldThrowException()
+        {
+            // Arrange
+            var discountHelper = GetDiscountHelper();
+            // Act, Assert
+            var discountedTotal = discountHelper.ApplyDiscount(-1);
+        }
+
+
+
+
+
+        private MinimumDiscountHelper GetDiscountHelper()
+        {
+            return new MinimumDiscountHelper();
+        }
+    }
+}
---
+++ ASP-NET-MVC/ProAspNetMVC4/Chapter06_EssentialTools/Chapter06_EssentialTools/Infrastructure/MinimumDiscountHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Chapter06_EssentialTools.Interfaces;
+
+namespace Chapter06_EssentialTools.Infrastructure
+{
+    public class MinimumDiscountHelper : IDiscountHelper
+    {
+        public decimal ApplyDiscount(decimal total)
+        {
+            if (total < 0)
+                throw new ArgumentOutOfRangeException(paramName: "total", message: "Oh no!");
+            if (total > 100)
+                return total - (0.1m* total);
+            if (total >= 10 && total <= 100)
+                return total - 5;
+            return total;
+        }
+    }
+}
