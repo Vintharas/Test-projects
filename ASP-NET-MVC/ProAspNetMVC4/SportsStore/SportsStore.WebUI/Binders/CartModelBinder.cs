8d724ede9fae65261abce7836526aea5f3e87113
vintharas <jaime.gg83@gmail.com>
2013-04-05 23:30:30 +0200

    Pro ASP.NET MVC4: Continued SportStore sampel by adding a custom binder.

---
+++ ASP-NET-MVC/ProAspNetMVC4/SportsStore/SportsStore.WebUI/Binders/CartModelBinder.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+using SportsStore.Domain.Entities;
+
+namespace SportsStore.WebUI.Binders
+{
+    public class CartModelBinder :IModelBinder
+    {
+        private const string sessionKey = "Cart";
+
+        public object BindModel(ControllerContext controllerContext,  // The ControllerContext provides all the information the Controller has (including the request)
+                                ModelBindingContext bindingContext) // The binding context gives information about the model object you are being asked to build and tools for making the binding process easier
+        {
+            // Get the Cart from the session
+            Cart cart = (Cart)controllerContext.HttpContext.Session[sessionKey];
+            // Create the Cart from the session
+            if (cart == null)
+            {
+                cart = new Cart();
+                controllerContext.HttpContext.Session[sessionKey] = cart;
+            }
+
+            return cart;
+        }
+    }
+}
